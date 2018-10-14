ed79900c0b4aece2a057112c2072bb3702c48a34
Mark Tomlin <Dygear@gmail.com>
2018-10-14 00:53:16 -0400

    Initial Commit

---
+++ oMI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oMI
+//
+//  Created by Mark Tomlin on 18-10-14.
+//  Copyright © 2018 MimoCAD, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
